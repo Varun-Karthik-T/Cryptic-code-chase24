94e870d4393986d207ca98548f8803b723c849e4
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-04-19 22:48:41 +0530

    Update App.jsx

--- cryptic-code-chase/src/App.jsx
+++ cryptic-code-chase/src/App.jsx
@@ -13,13 +13,13 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/loc0" element={<Question location={data.locations[0].location} nextClue={data.locations[0].nextclue} id={data.locations[0].id} />} />
-          <Route path="/loc1" element={<Question location={data.locations[1].location} nextClue={data.locations[1].nextclue} id={data.locations[1].id} />} />
-          <Route path="/loc2" element={<Question location={data.locations[2].location} nextClue={data.locations[2].nextclue} id={data.locations[2].id} />} />
-          <Route path="/loc3" element={<Question location={data.locations[3].location} nextClue={data.locations[3].nextclue} id={data.locations[3].id} />} />
-          <Route path="/loc4" element={<Question location={data.locations[4].location} nextClue={data.locations[4].nextclue} id={data.locations[4].id} />} />
-          <Route path="/loc5" element={<Question location={data.locations[5].location} nextClue={data.locations[5].nextclue} id={data.locations[5].id} />} />
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/loc0" element={<Question location={data.locations[0].location} nextClue={data.locations[0].nextclue} id={data.locations[0].id} />} />
+          <Route exact path="/loc1" element={<Question location={data.locations[1].location} nextClue={data.locations[1].nextclue} id={data.locations[1].id} />} />
+          <Route exact path="/loc2" element={<Question location={data.locations[2].location} nextClue={data.locations[2].nextclue} id={data.locations[2].id} />} />
+          <Route exact path="/loc3" element={<Question location={data.locations[3].location} nextClue={data.locations[3].nextclue} id={data.locations[3].id} />} />
+          <Route exact path="/loc4" element={<Question location={data.locations[4].location} nextClue={data.locations[4].nextclue} id={data.locations[4].id} />} />
+          <Route exact path="/loc5" element={<Question location={data.locations[5].location} nextClue={data.locations[5].nextclue} id={data.locations[5].id} />} />
         </Routes>
       </Router>
     </>
