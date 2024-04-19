703b2fce36b32dc09e75b33c19b8da6e24363823
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-04-19 22:30:11 +0530

    bug fix

--- cryptic-code-chase/src/App.jsx
+++ cryptic-code-chase/src/App.jsx
@@ -1,18 +1,21 @@
-import { Button } from "@/components/ui/button"
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
-import Question from "./pages/ques"
-import data from "./data.json"
+import { Route, Routes, BrowserRouter as Router,useNavigate } from "react-router-dom";
+import Question from "./pages/ques";
+import data from "./data.json";
 
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  return <p onClick={() => navigate('/loc1')}>Vanakkam da mapla</p>;
+};
 
 function App() {
-
   return (
     <>
       <Router>
         <Routes>
-          <Route path="https://cryptic-code-chase24.onrender.com/" element={ <p>Vanakkam da mapla</p>} />
-          <Route path="https://cryptic-code-chase24.onrender.com/loc0" element={<Question location={data.locations[0].location} nextClue={data.locations[0].nextclue} id={data.locations[0].id} />} />
-          <Route path="https://cryptic-code-chase24.onrender.com/loc1" element={<Question location={data.locations[1].location} nextClue={data.locations[1].nextclue} id={data.locations[1].id} />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/loc0" element={<Question location={data.locations[0].location} nextClue={data.locations[0].nextclue} id={data.locations[0].id} />} />
+          <Route path="/loc1" element={<Question location={data.locations[1].location} nextClue={data.locations[1].nextclue} id={data.locations[1].id} />} />
           <Route path="/loc2" element={<Question location={data.locations[2].location} nextClue={data.locations[2].nextclue} id={data.locations[2].id} />} />
           <Route path="/loc3" element={<Question location={data.locations[3].location} nextClue={data.locations[3].nextclue} id={data.locations[3].id} />} />
           <Route path="/loc4" element={<Question location={data.locations[4].location} nextClue={data.locations[4].nextclue} id={data.locations[4].id} />} />
@@ -20,7 +23,7 @@ function App() {
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
