5b79bfcd121fb6ec54f5d95b602bf41c7698a517
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-04-25 23:51:25 +0530

    version 1.0 commit

--- cryptic-code-chase/src/App.jsx
+++ cryptic-code-chase/src/App.jsx
@@ -19,12 +19,16 @@ function App() {
         <Router>
           <Routes>
             <Route exact path="/" element={<p>Vanakkam da mapla</p>} />
-            <Route exact path="/loc0" element={<Question location={data.locations[0].location} nextClue={data.locations[0].nextclue} id={data.locations[0].id} />} />
-            <Route exact path="/loc1" element={<Question location={data.locations[1].location} nextClue={data.locations[1].nextclue} id={data.locations[1].id} />} />
-            <Route exact path="/loc2" element={<Question location={data.locations[2].location} nextClue={data.locations[2].nextclue} id={data.locations[2].id} />} />
-            <Route exact path="/loc3" element={<Question location={data.locations[3].location} nextClue={data.locations[3].nextclue} id={data.locations[3].id} />} />
-            <Route exact path="/loc4" element={<Question location={data.locations[4].location} nextClue={data.locations[4].nextclue} id={data.locations[4].id} />} />
-            <Route exact path="/loc5" element={<Question location={data.locations[5].location} nextClue={data.locations[5].nextclue} id={data.locations[5].id} />} />
+            <Route exact path="/1toyota" element={<Question location={data.locations[0].location} nextClue={data.locations[0].nextclue} id={data.locations[0].id} />} />
+            <Route exact path="/2leyland" element={<Question location={data.locations[1].location} nextClue={data.locations[1].nextclue} id={data.locations[1].id} />} />
+            <Route exact path="/3benz" element={<Question location={data.locations[2].location} nextClue={data.locations[2].nextclue} id={data.locations[2].id} />} />
+            <Route exact path="/4bugatti" element={<Question location={data.locations[3].location} nextClue={data.locations[3].nextclue} id={data.locations[3].id} />} />
+            <Route exact path="/5maruti" element={<Question location={data.locations[4].location} nextClue={data.locations[4].nextclue} id={data.locations[4].id} />} />
+            <Route exact path="/6skoda" element={<Question location={data.locations[5].location} nextClue={data.locations[5].nextclue} id={data.locations[5].id} />} />
+            <Route exact path="/7bmw" element={<Question location={data.locations[6].location} nextClue={data.locations[6].nextclue} id={data.locations[6].id} />} />
+            <Route exact path="/8audi" element={<Question location={data.locations[7].location} nextClue={data.locations[7].nextclue} id={data.locations[7].id} />} />
+            <Route exact path="/9rolls" element={<Question location={data.locations[8].location} nextClue={data.locations[8].nextclue} id={data.locations[8].id} />} />
+            
           </Routes>
         </Router>
       </body>
--- cryptic-code-chase/src/pages/ques.jsx
+++ cryptic-code-chase/src/pages/ques.jsx
@@ -20,7 +20,29 @@ export default function Question({ location, nextClue, id }) {
 
 
     function questionPicker(team, id) {
-        return (((team % 5) + (id - 1)) % 5);
+
+        if(id<5){
+        console.log(team, id);
+
+        const temp = team % 4;
+        console.log(temp);
+        let a = [1, 2, 3, 4];
+        if (temp == 0) {
+            a = [1, 2, 3, 4];
+        }
+        else if (temp == 1) {
+            a = [3, 4, 1, 2];
+        }
+        else if (temp == 2) {
+            a = [2, 3, 4, 1];
+        }
+        else if (temp == 3) {
+            a = [4, 1, 2, 3];
+        }
+        console.log(a[id - 1]);
+        return a[id - 1];
+    }
+    else{return id;}
     }
 
 
@@ -57,11 +79,14 @@ export default function Question({ location, nextClue, id }) {
     };
 
 
+
+
     return (
         <>
             {!team && (
                 <div className='flex flex-col justify-center items-center h-screen w-full '>
                     <div className='flex flex-col items-center justify-center w-3/5 gap-8'>
+                        <p>Team Number</p>
                         <Input placeholder="Enter team number:" onChange={(e) => setTemp(e.target.value)} />
                         <Button variant="var1" onClick={() => temp > 0 && temp < 100 ? setTeam(temp) : alert("Enter a valid team number")}>Submit</Button>
                     </div>
@@ -69,10 +94,14 @@ export default function Question({ location, nextClue, id }) {
             )}
 
             {team && !solved && (
-                <div className='flex flex-col justify-center items-center h-screen w-full '>
-                    <div className="flex flex-col items-center justify-center w-4/5 gap-8">
-                        <p className='text-center text-white w-4/5 md:w-3/5'>Enter answer as "123". This is a sample question{questionPicker(team, id)}.{data.questions[questionPicker(team, id)].question}</p>
-                        <Input placeholder="Answer here" className="md:w-3/5 w-4/5" onChange={(e) => setTemp1(e.target.value)} />
+                <div className='flex flex-col justify-center items-center h-screen w-full text-center'>
+                    <div className="flex flex-col items-center justify-center w-4/5 gap-2 ">
+                        <p className=' text-white w-4/5 md:w-3/5'>{data.questions[questionPicker(team, id)].question}</p>
+                        {data.questions[questionPicker(team, id)].cipher && <><p className="text-yellow-500">Cipher/Code:</p><p className='text-white'>{data.questions[questionPicker(team, id)].cipher}</p></>}
+                        {data.questions[questionPicker(team, id)].key && <><p className='text-white'><span className="text-yellow-500">Key:</span>{data.questions[questionPicker(team, id)].key}</p></>}
+                        {data.questions[questionPicker(team, id)].hint && <><p className="text-yellow-500">Hint:</p><p className='text-white '>{data.questions[questionPicker(team, id)].hint}</p></>}
+
+                        <Input placeholder="Capital letters only" className="md:w-3/5 w-4/5" onChange={(e) => setTemp1(e.target.value)} />
                         <Button variant="var1" onClick={() => answerCheck(temp1)}>Submit</Button>
                     </div>
                 </div>
